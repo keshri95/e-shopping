8682200d12f1a1d9cadd5823c144e9a7fb5492e4
Ashish Keshri <keshri.ashish1@gmail.com>
2022-12-04 11:06:40 +0530

    add-remove-reducer-added

--- src/components/context/context.js
+++ src/components/context/context.js
@@ -2,12 +2,13 @@ import React, { useReducer } from "react";
 import { createContext, useContext } from "react";
 import { faker } from "@faker-js/faker";
 import reducer from "./reducer";
+// import { type } from "@testing-library/user-event/dist/type";
 
 const products = [...Array(20)].map(() => ({
   id: faker.datatype.uuid(),
-  name: faker.commerce.productName(),
+  name: faker.commerce.product(),
   price: faker.commerce.price(),
-  image: faker.image.fashion(),
+  image: faker.image.image(),
   firstName: faker.name.firstName(),
   inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
   fastDelivery: faker.datatype.boolean(),
@@ -24,7 +25,6 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initalState);
 
-  console.log(products);
   return (
     <>
       <AppContext.Provider value={{ state, dispatch }}>
--- src/components/context/reducer.js
+++ src/components/context/reducer.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const reducer = (state, action) => {
-  return (
-    <div>reducer</div>
-  )
-}
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, qty: 1 }],
+      };
 
-export default reducer
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+
+
+
+    default:
+      return state;
+  }
+
+  return <div>reducer</div>;
+};
+
+export default reducer;
